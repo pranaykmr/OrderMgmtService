1967a93da872e6f60e5279859ade6ce2c687b4ae
Pranay Kumar <pranay.kmr@live.com>
2019-05-27 22:58:28 +0530

    Conditional Get Order Info

--- OrderMgmtService/Controllers/OrderInfoController.cs
+++ OrderMgmtService/Controllers/OrderInfoController.cs
@@ -16,13 +16,14 @@ namespace OrderMgmtService.Controllers
         [ResponseType(typeof(OrderInfo[]))]
         [ActionName("GetOrderInfo")]
         [HttpGet]
-        public IHttpActionResult GetOrderInfo(string token)
+        public IHttpActionResult GetOrderInfo(string token, bool getInactiveOrders)
         {
-
+            if (getInactiveOrders) { 
             var orderInfo = (from o in db.Orders
                               join f in db.Factories on o.Factory_Id equals f.Factory_Id
                               join b in db.Buyers on o.BuyerId equals b.BuyerId
                               join s in db.ShippingModes on o.ShippingMode_Id equals s.ShippingMode_Id
+                              orderby o.Delivery descending
                               select new OrderInfo
                               {
                                   OrderNo = o.Order_No,
@@ -41,8 +42,36 @@ namespace OrderMgmtService.Controllers
                                   BuyerId = o.BuyerId,
                                   BuyerName = b.BuyerName,
                               });
-
-            return Ok(orderInfo);
+                return Ok(orderInfo);
+            }
+            else
+            {
+                var orderInfo = (from o in db.Orders
+                                 join f in db.Factories on o.Factory_Id equals f.Factory_Id
+                                 join b in db.Buyers on o.BuyerId equals b.BuyerId
+                                 join s in db.ShippingModes on o.ShippingMode_Id equals s.ShippingMode_Id
+                                 where o.Ship_Date != null
+                                 orderby o.Delivery descending
+                                 select new OrderInfo
+                                 {
+                                     OrderNo = o.Order_No,
+                                     StyleNo = o.Style_No,
+                                     Quantity = o.Quantity,
+                                     Delivery = o.Delivery,
+                                     FactoryId = o.Factory_Id,
+                                     FactoryName = f.Name,
+                                     PushraseOrderNo = o.Purchase_Order_No,
+                                     ShippingModeId = o.ShippingMode_Id,
+                                     ShippingModeName = s.ShippingModeName,
+                                     PriceFOB = o.Price_FOB,
+                                     FactoryPrice = o.Factory_Price,
+                                     TotalValue = o.Total_Value,
+                                     ShipDate = o.Ship_Date,
+                                     BuyerId = o.BuyerId,
+                                     BuyerName = b.BuyerName,
+                                 });
+                return Ok(orderInfo);
+            }
         }
 
     }
